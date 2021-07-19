6c55883f1df63ba0e9ab4e09e54cae04b5c0d9ec
Nikita Zlobin <nick87720z@gmail.com>
2021-07-20 00:45:54 +0500

    hex_char_to_int: crazy fast rewrite, found in rncbc's qsampler code
    One of cases, when playing smart only makes a slowdown.
    In my test, using multiple kilobytes of rand-generated data, this was
    4 to 8 times faster than all smart variants I could find.

--- src/util/common.c
+++ src/util/common.c
@@ -477,18 +477,30 @@ char *contract_tilde(const char *s)
 
 int hex_char_to_int(char c)
 {
-    int r;
-
-    if (c >= '0' && c <= '9')
-        r = c - '0';
-    else if (c >= 'a' && c <= 'f')
-        r = c - 'a' + 10;
-    else if (c >= 'A' && c <= 'F')
-        r = c - 'A' + 10;
-    else
-        r = 0;
-
-    return r;
+    switch (c) {
+    case '1': return 1;
+    case '2': return 2;
+    case '3': return 3;
+    case '4': return 4;
+    case '5': return 5;
+    case '6': return 6;
+    case '7': return 7;
+    case '8': return 8;
+    case '9': return 9;
+    case 'a': return 10;
+    case 'b': return 11;
+    case 'c': return 12;
+    case 'd': return 13;
+    case 'e': return 14;
+    case 'f': return 15;
+    case 'A': return 10;
+    case 'B': return 11;
+    case 'C': return 12;
+    case 'D': return 13;
+    case 'E': return 14;
+    case 'F': return 15;
+    default:  return 0;
+    }
 }
 
 int hex_to_rgb(char *hex, int *r, int *g, int *b)

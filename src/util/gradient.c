f7282a3d78068856f653db79e67ae9b6e370177a
Nikita Zlobin <nick87720z@gmail.com>
2021-08-04 02:21:24 +0500

    area.c: gradients refactoring

--- src/util/gradient.c
+++ src/util/gradient.c
@@ -17,32 +17,31 @@ GradientType gradient_type_from_string(const char *str)
 
 void init_gradient(GradientClass *g, GradientType type)
 {
-    #define add_gradient_offset(list,...) do {                 \
-        Offset *_offset = (Offset *)calloc(1, sizeof(Offset)); \
-        *_offset = (Offset){ __VA_ARGS__ };                    \
-        list = g_list_append((list), _offset);                 \
+    #define add_gradient_offset(offset,...) do {      \
+        offset = (Offset *)calloc(1, sizeof(Offset)); \
+        *offset = (Offset){ __VA_ARGS__ };            \
     } while(0)
 
     memset(g, 0, sizeof(*g));
     g->type = type;
     switch (type) {
     case GRADIENT_VERTICAL:
-        add_gradient_offset(g->from.offsets_y, .constant = TRUE, .constant_value = 0);
-        add_gradient_offset(g->to.offsets_y, .constant = FALSE, .element = ELEMENT_SELF, .variable = SIZE_HEIGHT, .multiplier = 1.0);
+        add_gradient_offset(g->from.offsets_y, .variable = SIZE_CONST, .constant_value = 0);
+        add_gradient_offset(g->to.offsets_y, .variable = SIZE_HEIGHT, .multiplier = 1.0);
         break;
     case GRADIENT_HORIZONTAL:
-        add_gradient_offset(g->from.offsets_x, .constant = TRUE, .constant_value = 0);
-        add_gradient_offset(g->to.offsets_x, .constant = FALSE, .element = ELEMENT_SELF, .variable = SIZE_WIDTH, .multiplier = 1.0);
+        add_gradient_offset(g->from.offsets_x, .variable = SIZE_CONST, .constant_value = 0);
+        add_gradient_offset(g->to.offsets_x, .variable = SIZE_WIDTH, .multiplier = 1.0);
         break;
     case GRADIENT_CENTERED:
         // from
-        add_gradient_offset(g->from.offsets_x, .constant = FALSE, .element = ELEMENT_SELF, .variable = SIZE_CENTERX, .multiplier = 1.0);
-        add_gradient_offset(g->from.offsets_y, .constant = FALSE, .element = ELEMENT_SELF, .variable = SIZE_CENTERY, .multiplier = 1.0);
-        add_gradient_offset(g->from.offsets_r, .constant = TRUE, .constant_value = 0);
+        add_gradient_offset(g->from.offsets_x, .variable = SIZE_CENTERX, .multiplier = 1.0);
+        add_gradient_offset(g->from.offsets_y, .variable = SIZE_CENTERY, .multiplier = 1.0);
+        add_gradient_offset(g->from.offsets_r, .variable = SIZE_CONST, .constant_value = 0);
         // to
-        add_gradient_offset(g->to.offsets_x, .constant = FALSE, .element = ELEMENT_SELF, .variable = SIZE_CENTERX, .multiplier = 1.0);
-        add_gradient_offset(g->to.offsets_y, .constant = FALSE, .element = ELEMENT_SELF, .variable = SIZE_CENTERY, .multiplier = 1.0);
-        add_gradient_offset(g->to.offsets_r, .constant = FALSE, .element = ELEMENT_SELF, .variable = SIZE_RADIUS,  .multiplier = 1.0);
+        add_gradient_offset(g->to.offsets_x, .variable = SIZE_CENTERX, .multiplier = 1.0);
+        add_gradient_offset(g->to.offsets_y, .variable = SIZE_CENTERY, .multiplier = 1.0);
+        add_gradient_offset(g->to.offsets_r, .variable = SIZE_RADIUS,  .multiplier = 1.0);
         break;
     }
 
@@ -52,11 +51,23 @@ void init_gradient(GradientClass *g, GradientType type)
 void cleanup_gradient(GradientClass *g)
 {
     g_list_free_full(g->extra_color_stops, free);
-    g_list_free_full(g->from.offsets_x, free);
-    g_list_free_full(g->from.offsets_y, free);
-    g_list_free_full(g->from.offsets_r, free);
-    g_list_free_full(g->to.offsets_x, free);
-    g_list_free_full(g->to.offsets_y, free);
-    g_list_free_full(g->to.offsets_r, free);
+    switch (g->type) {
+    case GRADIENT_VERTICAL:
+        free(g->from.offsets_y);
+        free(g->to.offsets_y);
+        break;
+    case GRADIENT_HORIZONTAL:
+        free(g->from.offsets_x);
+        free(g->to.offsets_x);
+        break;
+    case GRADIENT_CENTERED:
+        free(g->from.offsets_x);
+        free(g->from.offsets_y);
+        free(g->from.offsets_r);
+        free(g->to.offsets_x);
+        free(g->to.offsets_y);
+        free(g->to.offsets_r);
+        break;
+    }
     memset(g, 0, sizeof(*g));
 }

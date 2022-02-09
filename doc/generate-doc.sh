10f498ca34e11ff6f6a9a1d7754d9a73387ec158
Nikita Zlobin <nick87720z@gmail.com>
2022-02-09 20:04:47 +0500

    move html doc parts into subdir

--- doc/generate-doc.sh
+++ doc/generate-doc.sh
@@ -6,12 +6,12 @@
 
 set -e -x
 
-go-md2man -in=tint2.md > tint2.1
+go-md2man -in='tint2.md' > 'tint2.1'
 
-markdown -f githubtags < tint2.md |
-sed 's/^# TINT2 .*$/# TINT2/g'    |
-cat header.html - footer.html > manual.html
+markdown -f githubtags < 'tint2.md' |
+sed 's/^# TINT2 .*$/# TINT2/g'      |
+cat 'html_templ/header.html' - 'html_templ/footer.html' > 'manual.html'
 
-markdown -f githubtags < ../README.md |
-sed 's|doc/tint2.md|manual.html|g'    |
-cat header.html - footer.html > readme.html
+markdown -f githubtags < '../README.md' |
+sed 's|doc/tint2.md|manual.html|g'      |
+cat 'html_templ/header.html' - 'html_templ/footer.html' > 'readme.html'

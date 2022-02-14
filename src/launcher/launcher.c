nick87720z/tint2

commit 13b0ebd79bdf79a510cf02441994b994046274de

various optimizations

src/launcher/launcher.c
@@ -371,14 +371,15 @@ void draw_launcher_icon(void *obj, cairo_t *c)
 
     Imlib_Image image;
     // Render
-    if (panel_config.mouse_effects) {
-        if (launcherIcon->area.mouse_state == MOUSE_OVER)
-            image = launcherIcon->image_hover ? launcherIcon->image_hover : launcherIcon->image;
-        else if (launcherIcon->area.mouse_state == MOUSE_DOWN)
-            image = launcherIcon->image_pressed ? launcherIcon->image_pressed : launcherIcon->image;
-        else
-            image = launcherIcon->image;
-    } else {
+    if (!panel_config.mouse_effects)
+        goto im_default;
+    switch (launcherIcon->area.mouse_state) {
+    case MOUSE_OVER:
+        image = launcherIcon->image_hover ? launcherIcon->image_hover : launcherIcon->image;
+    case MOUSE_DOWN:
+        image = launcherIcon->image_pressed ? launcherIcon->image_pressed : launcherIcon->image;
+    default:
+    im_default:
         image = launcherIcon->image;
     }
     imlib_context_set_image(image);

src/button/button.c
@@ -273,6 +273,26 @@ void cleanup_button()
     panel_config.button_list = NULL;
 }
 
+int button_icon_desired_size(Button *button)
+{
+    int size;
+    Area *area = &button->area;
+    ButtonBackend *backend = button->backend;
+    if (backend->icon_name) {
+        int max_icon_size = backend->max_icon_size;
+        size = ( panel_horizontal ?
+            area->height - top_bottom_border_width(area) : 
+            area->width  - left_right_border_width(area) ) - 2 * area->paddingy;
+        if (max_icon_size) {
+            size = MIN(size, max_icon_size * ((Panel *)area->panel)->scale);
+        }
+    } else {
+        size = 0;
+    }
+    
+    return size;
+}
+
 int button_compute_desired_size(void *obj)
 {
     Button *button = obj;
@@ -285,19 +305,7 @@ int button_compute_desired_size(void *obj)
     int interior_padding = area->paddingx * panel->scale;
 
     int icon_w, icon_h;
-    if (backend->icon_name) {
-        int max_icon_size = backend->max_icon_size;
-        if (panel_horizontal)
-            icon_h = icon_w = area->height - top_bottom_border_width(area) - 2 * vert_padding;
-        else
-            icon_h = icon_w = area->width - left_right_border_width(area) - 2 * horiz_padding;
-        if (max_icon_size) {
-            icon_w = MIN(icon_w, max_icon_size * panel->scale);
-            icon_h = MIN(icon_h, max_icon_size * panel->scale);
-        }
-    } else {
-        icon_h = icon_w = 0;
-    }
+    icon_w = icon_h = button_icon_desired_size(button);
 
     int txt_height, txt_width;
     if (backend->text) {
@@ -366,18 +374,7 @@ gboolean resize_button(void *obj)
     int interior_padding = area->paddingx * panel->scale;
 
     int icon_w, icon_h;
-    if (backend->icon_name) {
-        if (panel_horizontal)
-            icon_h = icon_w = area->height - top_bottom_border_width(area) - 2 * vert_padding;
-        else
-            icon_h = icon_w = area->width - left_right_border_width(area) - 2 * horiz_padding;
-        if (backend->max_icon_size) {
-            icon_w = MIN(icon_w, backend->max_icon_size * panel->scale);
-            icon_h = MIN(icon_h, backend->max_icon_size * panel->scale);
-        }
-    } else {
-        icon_h = icon_w = 0;
-    }
+    icon_w = icon_h = button_icon_desired_size(button);
 
     frontend->iconw = icon_w;
     frontend->iconh = icon_h;
@@ -483,15 +480,18 @@ void draw_button(void *obj, cairo_t *c)
     if (frontend->icon) {
         // Render icon
         Imlib_Image image;
-        if (panel_config.mouse_effects) {
-            if (area->mouse_state == MOUSE_OVER)
-                image = frontend->icon_hover ? frontend->icon_hover : frontend->icon;
-            else if (area->mouse_state == MOUSE_DOWN)
-                image = frontend->icon_pressed ? frontend->icon_pressed : frontend->icon;
-            else
-                image = frontend->icon;
-        } else {
-            image = frontend->icon;
+        if (!panel_config.mouse_effects)
+            goto nofx;
+        switch (area->mouse_state) {
+        case MOUSE_OVER:    if (!frontend->icon_hover)
+                                goto nofx;
+                            image = frontend->icon_hover;
+                            break;
+        case MOUSE_DOWN:    if (!frontend->icon_pressed)
+                                goto nofx;
+                            image = frontend->icon_pressed;
+                            break;
+        default: nofx:      image = frontend->icon;
         }
 
         imlib_context_set_image(image);

src/drag_and_drop.c
@@ -229,12 +229,7 @@ void handle_dnd_position(XClientMessageEvent *e)
         task_handle_mouse_event(task, TOGGLE);
     } else {
         LauncherIcon *icon = click_launcher_icon(panel, mapX, mapY);
-        if (icon) {
-            accept = 1;
-            dnd_launcher_icon = icon;
-        } else {
-            dnd_launcher_icon = NULL;
-        }
+        dnd_launcher_icon = icon ? (accept = 1, icon) : NULL;
     }
 
     // send XdndStatus event to get more XdndPosition events

src/freespace/freespace.c
@@ -60,9 +60,10 @@ void init_freespace_panel(void *p)
 
 void cleanup_freespace(Panel *panel)
 {
-    if (panel->freespace_list)
+    if (panel->freespace_list) {
         g_list_free_full(panel->freespace_list, free);
-    panel->freespace_list = NULL;
+        panel->freespace_list = NULL;
+    }
 }
 
 int freespace_get_max_size(Panel *panel)
@@ -81,17 +82,11 @@ int freespace_get_max_size(Panel *panel)
             spacers++;
             continue;
         }
-
-        if (panel_horizontal)
-            size += a->width + panel->area.paddingx * panel->scale;
-        else
-            size += a->height + panel->area.paddingy * panel->scale;
+        size += panel_horizontal ? a->width  + panel->area.paddingx * panel->scale
+                                 : a->height + panel->area.paddingy * panel->scale;
     }
-
-    if (panel_horizontal)
-        size = panel->area.width - size - left_right_border_width(&panel->area) - panel->area.paddingxlr * panel->scale;
-    else
-        size = panel->area.height - size - top_bottom_border_width(&panel->area) - panel->area.paddingxlr * panel->scale;
+    size = (panel_horizontal ? panel->area.width  - left_right_border_width(&panel->area)
+                             : panel->area.height - top_bottom_border_width(&panel->area)) - size - panel->area.paddingxlr * panel->scale;
 
     return size / spacers;
 }

src/launcher/apps-common.c
@@ -46,11 +46,7 @@ int parse_dektop_line(char *line, char **key, char **value)
             break;
         }
     }
-    if (!found)
-        return 0;
-    if (found && (strlen(*key) == 0 || strlen(*value) == 0))
-        return 0;
-    return 1;
+    return found && strlen(*key) && strlen(*value);
 }
 
 void expand_exec(DesktopEntry *entry, const char *path)

src/launcher/icon-theme-common.c
@@ -510,35 +510,31 @@ IconThemeWrapper *load_themes(const char *icon_theme_name)
 
 int directory_matches_size(IconThemeDir *dir, int size)
 {
-    if (dir->type == ICON_DIR_TYPE_FIXED) {
+    switch (dir->type) {
+    case ICON_DIR_TYPE_FIXED:
         return dir->size == size;
-    } else if (dir->type == ICON_DIR_TYPE_SCALABLE) {
+    case ICON_DIR_TYPE_SCALABLE:
         return dir->min_size <= size && size <= dir->max_size;
-    } else /*if (dir->type == ICON_DIR_TYPE_THRESHOLD)*/ {
+    default:
         return dir->size - dir->threshold <= size && size <= dir->size + dir->threshold;
     }
 }
 
 int directory_size_distance(IconThemeDir *dir, int size)
 {
-    if (dir->type == ICON_DIR_TYPE_FIXED) {
+    switch (dir->type) {
+    case ICON_DIR_TYPE_FIXED:
         return abs(dir->size - size);
-    } else if (dir->type == ICON_DIR_TYPE_SCALABLE) {
-        if (size < dir->min_size) {
-            return dir->min_size - size;
-        } else if (size > dir->max_size) {
-            return size - dir->max_size;
-        } else {
-            return 0;
-        }
-    } else /*if (dir->type == ICON_DIR_TYPE_THRESHOLD)*/ {
-        if (size < dir->size - dir->threshold) {
-            return dir->min_size - size;
-        } else if (size > dir->size + dir->threshold) {
-            return size - dir->max_size;
-        } else {
-            return 0;
-        }
+    case ICON_DIR_TYPE_SCALABLE:
+        return
+            size < dir->min_size ? dir->min_size - size
+        :   size > dir->max_size ? size - dir->max_size
+        :   0;
+    default:
+        return
+            (size < dir->size - dir->threshold) ? dir->min_size - size
+        :   (size > dir->size + dir->threshold) ? size - dir->max_size
+        :   0;
     }
 }
 
@@ -552,9 +548,7 @@ gint compare_theme_directories(gconstpointer a, gconstpointer b, gpointer size_q
 
 Bool is_full_path(const char *s)
 {
-    if (!s)
-        return FALSE;
-    return s[0] == '/';
+    return s && s[0] == '/';
 }
 
 Bool file_exists(const char *path)

src/launcher/xsettings-client.c
@@ -80,12 +80,9 @@ static void notify_changes(XSettingsClient *client, XSettingsList *old_list)
     while (old_iter || new_iter) {
         int cmp;
 
-        if (old_iter && new_iter)
-            cmp = strcmp(old_iter->setting->name, new_iter->setting->name);
-        else if (old_iter)
-            cmp = -1;
-        else
-            cmp = 1;
+        cmp = !old_iter ? 1
+            : new_iter ? strcmp(old_iter->setting->name, new_iter->setting->name)
+            : -1;
 
         if (cmp < 0) {
             client->notify(old_iter->setting->name, XSETTINGS_ACTION_DELETED, NULL, client->cb_data);
@@ -122,10 +119,8 @@ static XSettingsResult fetch_card16(XSettingsBuffer *buffer, CARD16 *result)
     x = *(CARD16 *)buffer->pos;
     buffer->pos += 2;
 
-    if (buffer->byte_order == local_byte_order)
-        *result = x;
-    else
-        *result = (x << 8) | (x >> 8);
+    *result = (buffer->byte_order == local_byte_order) ?
+              x : (x << 8) | (x >> 8);
 
     return XSETTINGS_SUCCESS;
 }
@@ -152,10 +147,8 @@ static XSettingsResult fetch_card32(XSettingsBuffer *buffer, CARD32 *result)
     x = *(CARD32 *)buffer->pos;
     buffer->pos += 4;
 
-    if (buffer->byte_order == local_byte_order)
-        *result = x;
-    else
-        *result = (x << 24) | ((x & 0xff00) << 8) | ((x & 0xff0000) >> 8) | (x >> 24);
+    *result = (buffer->byte_order == local_byte_order) ? x
+            : (x << 24) | ((x & 0xff00) << 8) | ((x & 0xff0000) >> 8) | (x >> 24);
 
     return XSETTINGS_SUCCESS;
 }
@@ -444,11 +437,10 @@ void xsettings_client_destroy(XSettingsClient *client)
 XSettingsResult xsettings_client_get_setting(XSettingsClient *client, const char *name, XSettingsSetting **setting)
 {
     XSettingsSetting *search = xsettings_list_lookup(client->settings, name);
-    if (search) {
-        *setting = xsettings_setting_copy(search);
-        return *setting ? XSETTINGS_SUCCESS : XSETTINGS_NO_MEM;
-    } else
-        return XSETTINGS_NO_ENTRY;
+    return
+        !search ? XSETTINGS_NO_ENTRY
+    :   (*setting = xsettings_setting_copy(search)) ? XSETTINGS_SUCCESS
+    :   XSETTINGS_NO_MEM;
 }
 
 Bool xsettings_client_process_event(XSettingsClient *client, XEvent *xev)

commit a21c078a1bb17386fa8582159d336db3bd47dd80

err: forgot breaks in switch (regression in 13b0ebd7)

src/launcher/launcher.c
@@ -374,13 +374,12 @@ void draw_launcher_icon(void *obj, cairo_t *c)
     if (!panel_config.mouse_effects)
         goto im_default;
     switch (launcherIcon->area.mouse_state) {
-    case MOUSE_OVER:
-        image = launcherIcon->image_hover ? launcherIcon->image_hover : launcherIcon->image;
-    case MOUSE_DOWN:
-        image = launcherIcon->image_pressed ? launcherIcon->image_pressed : launcherIcon->image;
+    case MOUSE_OVER:    image = launcherIcon->image_hover   ? launcherIcon->image_hover   : launcherIcon->image;
+                        break;
+    case MOUSE_DOWN:    image = launcherIcon->image_pressed ? launcherIcon->image_pressed : launcherIcon->image;
+                        break;
     default:
-    im_default:
-        image = launcherIcon->image;
+    im_default:         image = launcherIcon->image;
     }
     imlib_context_set_image(image);
     render_image(launcherIcon->area.pix, 0, 0);
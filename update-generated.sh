f0f8c300a2a80ed481a73d85fdb075654236d553
Nikita Zlobin <nick87720z@gmail.com>
2022-02-17 01:25:01 +0500

    update-generated: some checks for input data

--- update-generated.sh
+++ update-generated.sh
@@ -3,4 +3,7 @@
 # Update generated sources, created by codegen.
 # Last ditch attempt to compromise with cmake ninja generator
 
+LANG=C sort -c src/config-keys-list &&
 codegen/srcpp LIST2CODE_CMD src/config-keys.c src/config-keys.h
+
+exit $?

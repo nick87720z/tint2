4687ec7537a98605a3f85c772ac2c13d3ea2822e
Nikita Zlobin <nick87720z@gmail.com>
2022-02-25 01:19:44 +0500

    uevent optimization

--- src/util/uevent.c
+++ src/util/uevent.c
@@ -37,89 +37,98 @@ int uevent_fd = -1;
 static struct sockaddr_nl nls;
 static GSList *notifiers = NULL;
 
-static const char *has_prefix(const char *str, const char *end, const char *prefix, size_t prefixlen)
-{
-    return  (end - str) < prefixlen || memcmp(str, prefix, prefixlen) != 0
-            ? NULL : str + prefixlen;
-}
+#define has_prefix(str, end, prefix, prefixlen) (                                        \
+    (end && (end - str) < prefixlen) || memcmp(str, prefix, prefixlen) != 0              \
+    ? NULL : str + prefixlen                                                             \
+)
 
 #define HAS_CONST_PREFIX(str, end, prefix) has_prefix((str), end, prefix, sizeof(prefix) - 1)
-
-static void uevent_param_free(gpointer data)
-{
-    struct uevent_parameter *param = data;
-    free(param->key);
-    free(param->val);
-    free(param);
-}
 
 static void uevent_free(struct uevent *ev)
 {
     free(ev->path);
     free(ev->subsystem);
-    g_slist_free_full(ev->params, uevent_param_free);
+    g_slist_free_full(ev->params, free);
     free(ev);
 }
 
-static struct uevent *uevent_new(char *buffer, int size)
+static int uevent_new(struct uevent *ev, char *buffer, int size)
+// Fills event structure at ev address.
+// Returns 1 on success , 0 on error.
 {
     gboolean first = TRUE;
 
-    if (size == 0)
-        return NULL;
+    if (!size || !ev)
+        return 0;
 
-    struct uevent *ev = calloc(1, sizeof(*ev));
-    if (!ev)
-        return NULL;
-
-    /* ensure nul termination required by strlen() */
+    /* must be null-terminated */
     buffer[size - 1] = '\0';
 
-    const char *s = buffer;
-    const char *end = s + size;
-    for (; s < end; s += strlen(s) + 1) {
-        if (first) {
+    const char *s   = buffer;
+    const char *end = buffer + size;
+    while (s < end) {
+        if (first)
+        {
             const char *p = strchr(s, '@');
             if (!p) {
-                /* error: kernel events contain @ */
-                /* triggered by udev events, though */
-                free(ev);
-                return NULL;
+                /* error: kernel events contain @, triggered by udev events, though */
+                return 0;
             }
-            ev->path = strdup(p + 1);
+            // remaining part is device path - duplicate it
+            p = strchr ((s=p+1), '\0') + 1;
+            int l = p - s;
+            ev->path = malloc (l);
+            memcpy (ev->path, s, l);
             first = FALSE;
-        } else {
+            s = p;
+        }
+        else
+        {
             const char *val;
             if ((val = HAS_CONST_PREFIX(s, end, "ACTION=")) != NULL) {
                 ev->action =
-                    !strcmp(val, "add") ? UEVENT_ADD
-                :   !strcmp(val, "remove") ? UEVENT_REMOVE
-                :   !strcmp(val, "change") ? UEVENT_CHANGE
-                : UEVENT_UNKNOWN;
+                    !strcmp(val, "add"   ) ? (s = val + sizeof("add"   ), UEVENT_ADD)
+                :   !strcmp(val, "remove") ? (s = val + sizeof("remove"), UEVENT_REMOVE)
+                :   !strcmp(val, "change") ? (s = val + sizeof("change"), UEVENT_CHANGE)
+                :   /* default ? */          (s = strchr (val, '\0') + 1, UEVENT_UNKNOWN);
             } else if ((val = HAS_CONST_PREFIX(s, end, "SEQNUM=")) != NULL) {
                 ev->sequence = atoi(val);
+                s = strchr (s, '\0') + 1;
             } else if ((val = HAS_CONST_PREFIX(s, end, "SUBSYSTEM=")) != NULL) {
-                ev->subsystem = strdup(val);
+                char *p = strchr (val, '\0') + 1;
+                int l = p - val;
+                ev->subsystem = malloc (l);
+                memcpy (ev->subsystem, val, l);
+                s = p;
             } else {
                 val = strchr(s, '=');
                 if (val) {
-                    struct uevent_parameter *param = malloc(sizeof(*param));
-                    if (param) {
-                        param->key = strndup(s, val - s);
-                        param->val = strdup(val + 1);
-                        ev->params = g_slist_append(ev->params, param);
+                    // total allocation for both structure and its strings
+                    // 3x less allocation headache
+                    char *p = strchr (val, '\0');
+                    int appendum = p - s + 1;
+                    struct uevent_parameter *param = malloc(sizeof(*param) + appendum);
+                    if (param)
+                    {
+                        param->key = (char *)(param + 1);
+                        memcpy (param->key, s, appendum);
+                        param->val = param->key + (val - s);
+                        param->val[-1] = '\0';
+                        ev->params = g_slist_prepend(ev->params, param);
                     }
-                }
+                    s = p + 1;
+                } else
+                    s = strchr(s, '\0') + 1;
             }
         }
     }
 
-    return ev;
+    return 1;
 }
 
 void uevent_register_notifier(struct uevent_notify *nb)
 {
-    notifiers = g_slist_append(notifiers, nb);
+    notifiers = g_slist_prepend(notifiers, nb);
 }
 
 void uevent_unregister_notifier(struct uevent_notify *nb)
@@ -147,21 +156,20 @@ void uevent_handler()
     if (len < 0)
         return;
 
-    struct uevent *ev = uevent_new(buf, len);
-    if (ev) {
-        for (GSList *l = notifiers; l; l = l->next) {
+    struct uevent ev;
+
+    if (uevent_new(&ev, buf, len)) {
+        for (GSList *l = notifiers; l; l = l->next)
+        {
             struct uevent_notify *nb = l->data;
 
-            if (!(ev->action & nb->action))
+            if (!(ev.action & nb->action) ||
+                nb->subsystem && strcmp(ev.subsystem, nb->subsystem) != 0)
+
                 continue;
 
-            if (nb->subsystem && strcmp(ev->subsystem, nb->subsystem))
-                continue;
-
-            nb->cb(ev, nb->userdata);
+            nb->cb(&ev, nb->userdata);
         }
-
-        uevent_free(ev);
     }
 }
 

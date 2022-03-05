nick87720z/tint2

commit 9f2d7eaac4248487c0939436bad7a34f829bc7a4

readability and optimization

src/util/server.c
@@ -459,8 +459,8 @@ void server_get_number_of_desktops()
 
     server.viewports = calloc(num_viewports, sizeof(Viewport));
     int k = 0;
-    for (int i = 0; i < MAX(x_screen_height / work_area_height, 1); i++) {
-        for (int j = 0; j < MAX(x_screen_width / work_area_width, 1); j++) {
+    for (int _max = MAX(x_screen_height / work_area_height, 1), i = 0; i < _max; i++) {
+        for (int j = 0; j < _max; j++) {
             server.viewports[k].x = j * work_area_width;
             server.viewports[k].y = i * work_area_height;
             server.viewports[k].width = work_area_width;

src/battery/battery.c
@@ -167,7 +167,7 @@ void battery_update_text(char *dest, char *format)
     // This ensures the string will always be null-terminated.
     memset(dest, 0, BATTERY_BUF_SIZE);
 
-    for (size_t o = 0; o < strlen(format); o++) {
+    for (size_t len = strlen(format), o = 0; o < len; o++) {
         char buf[BATTERY_BUF_SIZE];
         memset(buf, 0, BATTERY_BUF_SIZE);
         char *c = &format[o];

src/button/button.c
@@ -96,11 +96,11 @@ void destroy_button(void *obj)
 void init_button()
 {
     GList *to_remove = panel_config.button_list;
-    for (int k = 0; k < strlen(panel_items_order) && to_remove; k++) {
+
+    for_panel_items_order (&& to_remove)
         if (panel_items_order[k] == 'P') {
             to_remove = to_remove->next;
         }
-    }
 
     if (to_remove) {
         if (to_remove == panel_config.button_list) {

src/config.c
@@ -480,7 +480,7 @@ void add_entry(char *key, char *value)
 #endif
         clock_enabled = FALSE;
         taskbar_enabled = FALSE;
-        for (int j = strlen(panel_items_order); j; j--)
+        for (int items_n = strlen(panel_items_order), j = 0; j < items_n; j++)
             switch (panel_items_order[j]) {
             case 'L':   launcher_enabled = TRUE;
                         break;
@@ -1142,10 +1142,9 @@ void add_entry(char *key, char *value)
     case key_launcher_icon_size:
         launcher_max_icon_size = atoi(value);
         break;
-    case key_launcher_item_app: {
+    case key_launcher_item_app:
         panel_config.launcher.list_apps = g_slist_append(panel_config.launcher.list_apps, expand_tilde(value));
         break;
-    }
     case key_launcher_apps_dir: {
         char *path = expand_tilde(value);
         load_launcher_app_dir(path);

src/execplugin/execplugin.c
@@ -155,12 +155,10 @@ final_free:
 void init_execp()
 {
     GList *to_remove = panel_config.execp_list;
-    for (int k = 0, len = strlen(panel_items_order); k < len && to_remove; k++) {
+    for_panel_items_order (&& to_remove)
         if (panel_items_order[k] == 'E') {
             to_remove = to_remove->next;
         }
-    }
-
     if (to_remove) {
         if (to_remove == panel_config.execp_list) {
             g_list_free_full(to_remove, destroy_execp);

src/freespace/freespace.c
@@ -41,7 +41,7 @@ void init_freespace_panel(void *p)
         return;
 
     GList *fs_tail = NULL;
-    for (size_t k = 0; k < strlen(panel_items_order); k++)
+    for_panel_items_order ()
     {
         if (panel_items_order[k] == 'F') {
             FreeSpace *freespace = (FreeSpace *)calloc(1, sizeof(FreeSpace));

src/launcher/launcher.c
@@ -510,7 +510,7 @@ void launcher_reload_icon(Launcher *launcher, LauncherIcon *launcherIcon)
         launcherIcon->cwd = entry.cwd ? strdup(entry.cwd) : NULL;
         launcherIcon->start_in_terminal = entry.start_in_terminal;
         launcherIcon->startup_notification = entry.startup_notification;
-        launcherIcon->icon_name = entry.icon ? strdup(entry.icon) : strdup(DEFAULT_ICON);
+        launcherIcon->icon_name = strdup (entry.icon ? entry.icon : DEFAULT_ICON);
         if (entry.name)
             launcherIcon->icon_tooltip = entry.generic_name ? g_strdup_printf("%s (%s)", entry.name, entry.generic_name)
                                                             : g_strdup_printf("%s", entry.name);

src/panel.c
@@ -246,7 +246,7 @@ void init_panel()
         init_panel_size_and_position(p);
         area_gradients_create(&p->area);
         // add children according to panel_items
-        for (int k = 0; k < strlen(panel_items_order); k++) {
+        for_panel_items_order() {
             switch (panel_items_order[k]) {
             case 'L':   init_launcher_panel(p);
                         break;
@@ -671,7 +671,7 @@ void set_panel_items_order(Panel *p)
     int i_separator = 0;
     int i_freespace = 0;
     int i_button = 0;
-    for (int k = 0; k < strlen(panel_items_order); k++)
+    for_panel_items_order()
     {
         int i = p - panels;
         switch (panel_items_order[k]) {

commit 0163d2f36ef23534c555eaf44403665fe7c98c46

regression in commit 9f2d7eaa
Size of initialized work area

src/util/server.c
@@ -459,15 +459,15 @@ void server_get_number_of_desktops()
 
     server.viewports = calloc(num_viewports, sizeof(Viewport));
     int k = 0;
-    for (int _max = MAX(x_screen_height / work_area_height, 1), i = 0; i < _max; i++) {
-        for (int j = 0; j < _max; j++) {
+    for (int imax = MAX(x_screen_height / work_area_height, 1), i = 0; i < imax; i++)
+        for (int jmax = MAX(x_screen_width / work_area_width, 1), j = 0; j < jmax; j++)
+        {
             server.viewports[k].x = j * work_area_width;
             server.viewports[k].y = i * work_area_height;
             server.viewports[k].width = work_area_width;
             server.viewports[k].height = work_area_height;
             k++;
         }
-    }
 
     server.num_desktops = num_viewports;
 }
7aefeff92adb90d1e11da182c5265a854cc3232d
Nikita Zlobin <nick87720z@gmail.com>
2022-03-14 18:55:04 +0500

    update-generates.sh script update

--- update-generated.sh
+++ update-generated.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 #
-# Update generated sources, created by codegen.
-# Last ditch attempt to compromise with cmake ninja generator
+# Update generated sources.
+# For codegen output - last ditch attempt to compromise with cmake ninja generator
 
-LANG=C sort -c src/config-keys-list &&
-codegen/srcpp LIST2CODE_CMD src/config-keys.c src/config-keys.h
+selfdir=$( dirname $0 )
 
-exit $?
+LANG=C sort -c "${selfdir}"/src/config-keys-list &&
+"${selfdir}"/codegen/srcpp LIST2CODE_CMD "${selfdir}"/src/config-keys.c "${selfdir}"/src/config-keys.h
+
+code=$?
+if [ ${code} -ne 0 ]; then
+    exit ${code}
+fi
+
+command -v tint2 >/dev/null 2>&1 && [ "${selfdir}"/default_icon.png -nt "${selfdir}"/src/default_icon ] &&
+cd "${selfdir}"/src && tint2 --dump-image-data ../default_icon.png default_icon
+
+code=$?
+if [ ${code} -ne 0 ]; then
+    exit ${code}
+fi

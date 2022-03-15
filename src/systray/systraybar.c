nick87720z/tint2

commit 836e9e315924559e7e4d961f5cef97d1b976b595

allocation cleanup, prefer regular malloc instead of glib's one

src/systray/systraybar.c
@@ -502,14 +502,10 @@ void stop_net()
 {
     if (systray_profile)
         fprintf(stderr, "tint2: [%f] %s:%d\n", profiling_get_time(), __func__, __LINE__);
-    if (systray.list_icons) {
-        // remove_icon change systray.list_icons
-        while (systray.list_icons)
-            remove_icon((TrayWindow *)systray.list_icons->data);
 
-        g_slist_free(systray.list_icons);
-        systray.list_icons = NULL;
-    }
+    // remove_icon change systray.list_icons
+    while (systray.list_icons)
+        remove_icon((TrayWindow *)systray.list_icons->data);
 
     if (net_sel_win != None) {
         XDestroyWindow(server.display, net_sel_win);
@@ -704,7 +700,7 @@ gboolean add_icon(Window win)
                                   &set_attr);
 
     // Add the icon to the list
-    TrayWindow *traywin = g_new0(TrayWindow, 1);
+    TrayWindow *traywin = calloc( 1, sizeof(TrayWindow));
     traywin->parent = parent;
     traywin->win = win;
     traywin->depth = attr.depth;
@@ -909,15 +905,10 @@ void remove_icon(TrayWindow *traywin)
         imlib_context_set_image(traywin->image);
         imlib_free_image_and_decache();
     }
-    g_free(traywin);
+    free( traywin);
 
     // check empty systray
-    int count = 0;
-    GSList *l;
-    for (l = systray.list_icons; l; l = l->next) {
-        count++;
-    }
-    if (count == 0)
+    if (systray.list_icons)
         hide(&systray.area);
 
     // Resize and redraw the systray

src/battery/linux.c
@@ -98,7 +98,7 @@ static guint8 level_to_percent(gint level_now, gint level_full)
 
 static enum psy_type power_supply_get_type(const gchar *entryname)
 {
-    gchar *path_type = g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "type", NULL);
+    gchar *path_type = strdup_printf( NULL, "%s/sys/class/power_supply/%s/type", battery_sys_prefix, entryname);
     GError *error = NULL;
     gchar *type;
     gsize typelen;
@@ -106,11 +106,11 @@ static enum psy_type power_supply_get_type(const gchar *entryname)
     g_file_get_contents(path_type, &type, &typelen, &error);
     if (error) {
         fprintf(stderr, RED "tint2: %s:%d: read failed for %s" RESET "\n", __FILE__, __LINE__, path_type);
-        g_free(path_type);
+        free( path_type);
         g_error_free(error);
         return PSY_UNKNOWN;
     }
-    g_free(path_type);
+    free( path_type);
 
     if (!g_strcmp0(type, "Battery\n")) {
         g_free(type);
@@ -131,70 +131,53 @@ static gboolean init_linux_battery(struct psy_battery *bat)
 {
     const gchar *entryname = bat->name;
 
-    bat->path_present = g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "present", NULL);
+    bat->path_present = strdup_printf( NULL, "%s/sys/class/power_supply/%s/present", battery_sys_prefix, entryname);
     if (!is_file_non_empty(bat->path_present)) {
         fprintf(stderr, RED "tint2: %s:%d: read failed for %s" RESET "\n", __FILE__, __LINE__, bat->path_present);
-        goto err0;
+        return FALSE;
     }
 
-    bat->path_level_now =
-        g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "energy_now", NULL);
-    bat->path_level_full =
-        g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "energy_full", NULL);
-    bat->path_rate_now = g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "power_now", NULL);
+    bat->path_level_now = strdup_printf( NULL, "%s/sys/class/power_supply/%s/energy_now", battery_sys_prefix, entryname);
+    bat->path_level_full = strdup_printf( NULL, "%s/sys/class/power_supply/%s/energy_full", battery_sys_prefix, entryname);
+    bat->path_rate_now = strdup_printf( NULL, "%s/sys/class/power_supply/%s/power_now", battery_sys_prefix, entryname);
     bat->unit = 'W';
 
     if (!is_file_non_empty(bat->path_level_now) ||
         !is_file_non_empty(bat->path_level_full))
     {
-        g_free(bat->path_level_now);
-        g_free(bat->path_level_full);
-        g_free(bat->path_rate_now);
-        bat->path_level_now =
-            g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "charge_now", NULL);
-        bat->path_level_full =
-            g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "charge_full", NULL);
-        bat->path_rate_now =
-            g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "current_now", NULL);
+        free( bat->path_level_now);
+        free( bat->path_level_full);
+        free( bat->path_rate_now);
+
+        bat->path_level_now = strdup_printf( NULL, "%s/sys/class/power_supply/%s/charge_now", battery_sys_prefix, entryname);
+        bat->path_level_full = strdup_printf( NULL, "%s/sys/class/power_supply/%s/charge_full", battery_sys_prefix, entryname);
+        bat->path_rate_now = strdup_printf( NULL, "%s/sys/class/power_supply/%s/current_now", battery_sys_prefix, entryname);
         bat->unit = 'A';
     }
     if (!is_file_non_empty(bat->path_level_now)) {
         fprintf(stderr, RED "tint2: %s:%d: read failed for %s" RESET "\n", __FILE__, __LINE__, bat->path_level_now);
-        goto err1;
+        return FALSE;
     }
     if (!is_file_non_empty(bat->path_level_full)) {
         fprintf(stderr, RED "tint2: %s:%d: read failed for %s" RESET "\n", __FILE__, __LINE__, bat->path_level_full);
-        goto err1;
+        return FALSE;
     }
 
-    bat->path_status = g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "status", NULL);
+    bat->path_status = strdup_printf( NULL, "%s/sys/class/power_supply/%s/status", battery_sys_prefix, entryname);
     if (!is_file_non_empty(bat->path_status)) {
         fprintf(stderr, RED "tint2: %s:%d: read failed for %s" RESET "\n", __FILE__, __LINE__, bat->path_status);
-        goto err2;
+        return FALSE;
     }
 
     return TRUE;
-
-err2:
-    g_free(bat->path_status);
-err1:
-    g_free(bat->path_level_now);
-    g_free(bat->path_level_full);
-    g_free(bat->path_rate_now);
-err0:
-    g_free(bat->path_present);
-
-    return FALSE;
 }
 
 static gboolean init_linux_mains(struct psy_mains *ac)
 {
     const gchar *entryname = ac->name;
-
-    ac->path_online = g_build_filename(battery_sys_prefix, "/sys/class/power_supply", entryname, "online", NULL);
+    ac->path_online = strdup_printf( NULL, "%s/sys/class/power_supply/%s/online", battery_sys_prefix, entryname);
     if (!is_file_non_empty(ac->path_online)) {
         fprintf(stderr, RED "tint2: %s:%d: read failed for %s" RESET "\n", __FILE__, __LINE__, ac->path_online);
-        g_free(ac->path_online);
         return FALSE;
     }
 
@@ -204,21 +187,21 @@ static gboolean init_linux_mains(struct psy_mains *ac)
 static void psy_battery_free(gpointer data)
 {
     struct psy_battery *bat = data;
-    g_free(bat->name);
-    g_free(bat->path_status);
-    g_free(bat->path_rate_now);
-    g_free(bat->path_level_full);
-    g_free(bat->path_level_now);
-    g_free(bat->path_present);
-    g_free(bat);
+    free( bat->name);
+    free( bat->path_status);
+    free( bat->path_rate_now);
+    free( bat->path_level_full);
+    free( bat->path_level_now);
+    free( bat->path_present);
+    free( bat);
 }
 
 static void psy_mains_free(gpointer data)
 {
     struct psy_mains *ac = data;
-    g_free(ac->name);
-    g_free(ac->path_online);
-    g_free(ac);
+    free( ac->name);
+    free( ac->path_online);
+    free( ac);
 }
 
 void battery_os_free()
@@ -234,28 +217,28 @@ void battery_os_free()
 
 static void add_battery(const char *entryname)
 {
-    struct psy_battery *bat = g_malloc0(sizeof(*bat));
-    bat->name = g_strdup(entryname);
+    struct psy_battery *bat = calloc( 1, sizeof(*bat));
+    bat->name = strdup( entryname);
 
     if (init_linux_battery(bat)) {
         batteries = g_list_append(batteries, bat);
         fprintf(stderr, GREEN "Found battery \"%s\"" RESET "\n", bat->name);
     } else {
-        g_free(bat);
+        psy_battery_free( bat);
         fprintf(stderr, RED "tint2: Failed to initialize battery \"%s\"" RESET "\n", entryname);
     }
 }
 
 static void add_mains(const char *entryname)
 {
-    struct psy_mains *ac = g_malloc0(sizeof(*ac));
-    ac->name = g_strdup(entryname);
+    struct psy_mains *ac = calloc( 1, sizeof(*ac));
+    ac->name = strdup( entryname);
 
     if (init_linux_mains(ac)) {
         mains = g_list_append(mains, ac);
         fprintf(stderr, GREEN "Found mains \"%s\"" RESET "\n", ac->name);
     } else {
-        g_free(ac);
+        psy_mains_free( ac);
         fprintf(stderr, RED "tint2: Failed to initialize mains \"%s\"" RESET "\n", entryname);
     }
 }
@@ -268,9 +251,9 @@ gboolean battery_os_init()
 
     battery_os_free();
 
-    gchar *dir_path = g_build_filename(battery_sys_prefix, "/sys/class/power_supply", NULL);
+    gchar *dir_path = strdup_printf( NULL, "%s/sys/class/power_supply", battery_sys_prefix);
     directory = g_dir_open(dir_path, 0, &error);
-    g_free(dir_path);
+    free( dir_path);
     RETURN_ON_ERROR(error);
 
     while ((entryname = g_dir_read_name(directory))) {
@@ -471,23 +454,24 @@ static gchar *level_human_readable(struct psy_battery *bat)
     gint now  = bat->level_now;
     gint full = bat->level_full;
 
-    if (full >= 1000000) {
-        return g_strdup_printf("%d.%d / %d.%d %ch",
+    if (full >= 1000000)
+        return strdup_printf(  NULL,
+                               "%d.%d / %d.%d %ch",
                                now / 1000000,
                                (now % 1000000) / 100000,
                                full / 1000000,
                                (full % 1000000) / 100000,
                                bat->unit);
-    } else if (full >= 1000) {
-        return g_strdup_printf("%d.%d / %d.%d m%ch",
+    else if (full >= 1000)
+        return strdup_printf(  NULL,
+                               "%d.%d / %d.%d m%ch",
                                now / 1000,
                                (now % 1000) / 100,
                                full / 1000,
                                (full % 1000) / 100,
                                bat->unit);
-    } else {
-        return g_strdup_printf("%d / %d µ%ch", now, full, bat->unit);
-    }
+    else
+        return strdup_printf( NULL, "%d / %d µ%ch", now, full, bat->unit);
 }
 
 static gchar *rate_human_readable(struct psy_battery *bat)
@@ -495,10 +479,10 @@ static gchar *rate_human_readable(struct psy_battery *bat)
     gint rate = bat->rate_now;
     gchar unit = bat->unit;
 
-    return  (rate >= 1000000) ? g_strdup_printf("%d.%d %c", rate / 1000000, (rate % 1000000) / 100000, unit) :
-            (rate >= 1000   ) ? g_strdup_printf("%d.%d m%c", rate / 1000, (rate % 1000) / 100, unit) :
-            (rate > 0)        ? g_strdup_printf("%d µ%c", rate, unit) :
-                                g_strdup_printf("0 %c", unit);
+    return  (rate >= 1000000) ? strdup_printf( NULL, "%d.%d %c", rate / 1000000, (rate % 1000000) / 100000, unit) :
+            (rate >= 1000   ) ? strdup_printf( NULL, "%d.%d m%c", rate / 1000, (rate % 1000) / 100, unit) :
+            (rate > 0)        ? strdup_printf( NULL, "%d µ%c", rate, unit) :
+                                strdup_printf( NULL, "0 %c", unit);
 }
 
 char *battery_os_tooltip()
@@ -528,8 +512,8 @@ char *battery_os_tooltip()
 
         g_string_append_printf(tooltip, "\t%s: %s (%u %%)\n\trate: %s", state, level, percentage, rate);
 
-        g_free(rate);
-        g_free(level);
+        free( rate);
+        free( level);
     }
 
     for (l = mains; l != NULL; l = l->next) {

src/config.c
@@ -201,34 +201,46 @@ void load_launcher_app_dir(const char *path)
 
     GDir *d = g_dir_open(path, 0, NULL);
     if (d) {
+        size_t  path_len = strlen (path),
+                buf_len = 0;
         const gchar *name;
-        while ((name = g_dir_read_name(d))) {
-            gchar *file = g_build_filename(path, name, NULL);
+        gchar *file = NULL;
+        while ((name = g_dir_read_name(d)))
+        {
+            size_t _buf_len = path_len + 1 + strlen (name) + 1;
+            if (! file || _buf_len > buf_len) {
+                file = realloc( file, (buf_len = _buf_len));
+                file [buf_len-1] = '\0';
+            }
+            snprintf (file, buf_len - 1, "%s/%s", path, name);
             if (!g_file_test(file, G_FILE_TEST_IS_DIR) && g_str_has_suffix(file, ".desktop")) {
                 files = g_list_prepend(files, file);
+                file = NULL;
             } else if (g_file_test(file, G_FILE_TEST_IS_DIR)) {
                 subdirs = g_list_prepend(subdirs, file);
-            } else {
-                g_free(file);
-            }
+                file = NULL;
+            } else
+                free( file);
         }
+        if (file)
+            free( file);
         g_dir_close(d);
     }
 
     subdirs = g_list_sort(subdirs, compare_strings);
-    GList *l;
-    for (l = subdirs; l; l = l->next) {
+    for (GList *l = subdirs; l; l = l->next)
+    {
         gchar *dir = (gchar *)l->data;
         load_launcher_app_dir(dir);
-        g_free(dir);
+        free( dir);
     }
     g_list_free(subdirs);
 
     files = g_list_sort(files, compare_strings);
-    for (l = files; l; l = l->next) {
+    for (GList *l = files; l; l = l->next)
+    {
         gchar *file = (gchar *)l->data;
-        panel_config.launcher.list_apps = g_slist_append(panel_config.launcher.list_apps, strdup(file));
-        g_free(file);
+        panel_config.launcher.list_apps = g_slist_append( panel_config.launcher.list_apps, file);
     }
     g_list_free(files);
 }
@@ -1408,7 +1420,7 @@ gboolean config_read_default_path()
                 fclose(f);
             }
         }
-        g_free(syspath);
+        free( syspath);
     }
 done:;
     gboolean result = config_read_file(userpath);

src/init.c
@@ -234,10 +234,10 @@ void load_default_task_icon()
     const gchar *const *data_dirs = g_get_system_data_dirs();
     for (int i = 0; data_dirs[i] != NULL; i++)
     {
-        gchar *path = g_build_filename(data_dirs[i], "tint2", "default_icon.png", NULL);
+        gchar *path = strdup_printf( NULL, "%s/tint2/default_icon.png", data_dirs[i]);
         if (g_file_test(path, G_FILE_TEST_EXISTS))
             default_icon = load_image(path, TRUE);
-        g_free(path);
+        free( path);
     }
     if (!default_icon)
         default_icon = imlib_create_image_using_data(default_icon_width,

src/launcher/icon-theme-common.c
@@ -215,9 +215,9 @@ IconTheme *load_theme_from_index(const char *file_name, const char *name)
 
 void load_theme_from_fs_dir(IconTheme *theme, const char *dir_name)
 {
-    gchar *file_name = g_build_filename(dir_name, "index.theme", NULL);
+    gchar *file_name = strdup_printf( NULL, "%s/index.theme", dir_name);
     if (g_file_test(file_name, G_FILE_TEST_EXISTS)) {
-        g_free(file_name);
+        free( file_name);
         return;
     }
 
@@ -228,7 +228,7 @@ void load_theme_from_fs_dir(IconTheme *theme, const char *dir_name)
         const gchar *size_name;
         while ((size_name = g_dir_read_name(d)))
         {
-            gchar *full_size_name = g_build_filename(dir_name, size_name, NULL);
+            gchar *full_size_name = strdup_printf( NULL, "%s/%s", dir_name, size_name);
             if (g_file_test(file_name, G_FILE_TEST_IS_DIR)) {
                 int size, size2;
                 if ((sscanf(size_name, "%dx%d", &size, &size2) == 2 && size == size2) ||
@@ -240,9 +240,7 @@ void load_theme_from_fs_dir(IconTheme *theme, const char *dir_name)
                         {
                             IconThemeDir *dir = calloc(1, sizeof(IconThemeDir));
                             // value is like 48x48/apps
-                            gchar *value = g_build_filename(size_name, subdir_name, NULL);
-                            dir->name = strdup(value);
-                            g_free(value);
+                            dir->name = strdup_printf( NULL, "%s/%s", size_name, subdir_name);
                             dir->max_size = dir->min_size = dir->size = size;
                             dir->type = ICON_DIR_TYPE_FIXED;
                             g_slist_append_tail (theme->list_directories, d_tail, dir);
@@ -251,26 +249,26 @@ void load_theme_from_fs_dir(IconTheme *theme, const char *dir_name)
                     }
                 }
             }
-            g_free(full_size_name);
+            free( full_size_name);
         }
         g_dir_close(d);
     }
-    g_free( file_name);
+    free( file_name);
 }
 
 IconTheme *load_theme_from_fs(const char *name, IconTheme *theme)
 {
     gchar *dir_name = NULL;
-    for (const GSList *location = get_icon_locations(); location; location = location->next) {
+    for (const GSList *location = get_icon_locations(); location; location = location->next)
+    {
         gchar *path = (gchar *)location->data;
-        dir_name = g_build_filename(path, name, NULL);
+        dir_name = strdup_printf( NULL, "%s/%s", path, name);
         if (g_file_test(dir_name, G_FILE_TEST_IS_DIR)) {
-            if (!theme) {
+            if (!theme)
                 theme = make_theme(name);
-            }
             load_theme_from_fs_dir(theme, dir_name);
         }
-        g_free(dir_name);
+        free( dir_name);
         dir_name = NULL;
     }
 
@@ -290,10 +288,10 @@ IconTheme *load_theme(const char *name)
     for (const GSList *location = get_icon_locations(); location; location = location->next)
     {
         gchar *path = (gchar *)location->data;
-        file_name = g_build_filename(path, name, "index.theme", NULL);
+        file_name = strdup_printf( NULL, "%s/%s/index.theme", path, name);
         if (g_file_test(file_name, G_FILE_TEST_EXISTS))
             break;
-        g_free (file_name);
+        free( file_name);
         file_name = NULL;
     }
 
@@ -301,7 +299,7 @@ IconTheme *load_theme(const char *name)
 
     if (file_name) {
         theme = load_theme_from_index(file_name, name);
-        g_free (file_name);
+        free( file_name);
     } else
         theme = NULL;
 
@@ -457,11 +455,11 @@ void load_fallbacks(IconThemeWrapper *wrapper)
         if (d) {
             const gchar *name;
             while ((name = g_dir_read_name(d))) {
-                gchar *file_name = g_build_filename(path, name, "index.theme", NULL);
+                gchar *file_name = strdup_printf( NULL, "%s/%s/index.theme", path, name);
                 if (g_file_test(file_name, G_FILE_TEST_EXISTS) && !g_file_test(file_name, G_FILE_TEST_IS_DIR)) {
                     load_themes_helper(name, &wrapper->themes_fallback, &wrapper->_queued);
                 }
-                g_free(file_name);
+                free( file_name);
             }
             g_dir_close(d);
         }
@@ -746,10 +744,12 @@ char *get_icon_path_from_cache(IconThemeWrapper *wrapper, const char *icon_name,
         return NULL;
 
     load_icon_cache(wrapper);
+    
+    fprintf (stderr, "get_icon_path_from_cache: '%s' '%s'\n", wrapper->icon_theme_name, icon_name);
 
-    gchar *key = g_strdup_printf("%s\t%s\t%d", wrapper->icon_theme_name, icon_name, size);
+    gchar *key = strdup_printf( NULL, "%s\t%s\t%d", wrapper->icon_theme_name, icon_name, size);
     const gchar *value = get_from_cache(&wrapper->_cache, key);
-    g_free(key);
+    free( key);
 
     if (!value) {
         fprintf(stderr,
@@ -783,9 +783,9 @@ void add_icon_path_to_cache(IconThemeWrapper *wrapper, const char *icon_name, in
 
     load_icon_cache(wrapper);
 
-    gchar *key = g_strdup_printf("%s\t%s\t%d", wrapper->icon_theme_name, icon_name, size);
+    gchar *key = strdup_printf( NULL, "%s\t%s\t%d", wrapper->icon_theme_name, icon_name, size);
     add_to_cache(&wrapper->_cache, key, path);
-    g_free(key);
+    free( key);
 }
 
 char *get_icon_path(IconThemeWrapper *wrapper, const char *icon_name, int size, gboolean use_fallbacks)

src/taskbar/task.c
@@ -299,12 +299,12 @@ Imlib_Image task_get_icon(Window win, int icon_size)
                 int array_size = w * h;
                 // imlib needs the array in DATA32 type
                 // using malloc for the array to protect from stack overflow
-                DATA32 *icon_data = (DATA32*) g_try_malloc(sizeof(*icon_data) * array_size);
+                DATA32 *icon_data = malloc( sizeof(*icon_data) * array_size);
                 if (icon_data) {
                     for (int j = 0; j < array_size; ++j)
                         icon_data[j] = tmp_data[j];
                     img = imlib_create_image_using_copied_data(w, h, icon_data);
-                    g_free(icon_data);
+                    free( icon_data);
                 }
             }
         }

src/taskbar/taskbarname.c
@@ -67,19 +67,16 @@ void init_taskbarname_panel(void *p)
 
         // use desktop number if name is missing
         if (l) {
-            taskbar->bar_name.name = g_strdup(l->data);
+            taskbar->bar_name.name = strdup(l->data);
             l = l->next;
         } else
-            taskbar->bar_name.name = g_strdup_printf("%d", j + 1);
+            taskbar->bar_name.name = strdup_printf( NULL, "%d", j + 1);
 
         // append the name at the beginning of taskbar
         taskbar->area.children = g_list_append(taskbar->area.children, &taskbar->bar_name);
         area_gradients_create(&taskbar->bar_name.area);
     }
-
-    for (l = list; l; l = l->next)
-        g_free(l->data);
-    g_slist_free(list);
+    g_slist_free_full( list, free);
 }
 
 void taskbarname_init_fonts()
@@ -117,8 +114,7 @@ void cleanup_taskbarname()
         for (int j = 0; j < panel->num_desktops; j++)
         {
             Taskbar *taskbar = &panel->taskbar[j];
-            g_free(taskbar->bar_name.name);
-            taskbar->bar_name.name = NULL;
+            free_and_null( taskbar->bar_name.name);
             free_area(&taskbar->bar_name.area);
             remove_area((Area *)&taskbar->bar_name);
         }
@@ -227,21 +223,20 @@ void update_desktop_names()
         {
             gchar *name;
             if (l) {
-                name = g_strdup(l->data);
+                name = strdup( l->data);
                 l = l->next;
             } else
-                name = g_strdup_printf("%d", j + 1);
+                name = strdup_printf( NULL, "%d", j + 1);
+
             Taskbar *taskbar = &panels[i].taskbar[j];
             if (strcmp(name, taskbar->bar_name.name) != 0) {
-                g_free(taskbar->bar_name.name);
+                free( taskbar->bar_name.name);
                 taskbar->bar_name.name = name;
                 taskbar->bar_name.area.resize_needed = TRUE;
             } else
-                g_free(name);
+                free( name);
         }
     }
-    for (GSList *l = list; l; l = l->next)
-        g_free(l->data);
-    g_slist_free(list);
+    g_slist_free_full( list, free);
     schedule_panel_redraw();
 }

src/tint2conf/main.c
@@ -40,9 +40,8 @@ static void load_specific_themes(char **paths, int count);
 
 gchar *get_home_config_path()
 // Returns ~/.config/tint2/tint2rc (or equivalent).
-// Needs gfree.
 {
-    return g_build_filename((fetch_user_config_dir(), user_config_dir), "tint2", "tint2rc", NULL);
+    return strdup_printf( NULL, "%s/tint2/tint2rc", (fetch_user_config_dir(), user_config_dir));
 }
 
 gchar *get_etc_config_path()
@@ -641,9 +640,9 @@ static void theme_selection_changed(GtkWidget *treeview, gpointer userdata)
         gchar *filepath;
         gtk_tree_model_get(model, &iter, COL_THEME_FILE, &filepath, -1);
         gboolean isdefault = theme_is_default(filepath);
-        gchar *text = isdefault ? g_strdup_printf("tint2") : g_strdup_printf("tint2 -c %s", filepath);
+        gchar *text = isdefault ? strdup_printf( NULL, "tint2") : strdup_printf( NULL, "tint2 -c %s", filepath);
         gtk_entry_set_text(GTK_ENTRY(tint_cmd), text);
-        g_free(text);
+        free( text);
         gboolean editable = theme_is_editable(filepath);
         g_free(filepath);
         g_simple_action_set_enabled(ThemeSaveAs,        TRUE);
@@ -735,7 +734,7 @@ static void make_selected_theme_default()
     gchar *default_path = get_home_config_path();
     if (g_str_equal(filepath, default_path)) {
         g_free(filepath);
-        g_free(default_path);
+        free( default_path);
         return;
     }
 
@@ -749,7 +748,7 @@ static void make_selected_theme_default()
     gtk_widget_destroy(w);
     if (response != GTK_RESPONSE_YES) {
         g_free(filepath);
-        g_free(default_path);
+        free( default_path);
         return;
     }
 
@@ -757,7 +756,7 @@ static void make_selected_theme_default()
     select_first_theme();
 
     g_free(filepath);
-    g_free(default_path);
+    free( default_path);
 }
 
 static void viewRowActivated(GtkTreeView *tree_view, GtkTreePath *path, GtkTreeViewColumn *column, gpointer user_data)
@@ -770,19 +769,20 @@ static void viewRowActivated(GtkTreeView *tree_view, GtkTreePath *path, GtkTreeV
 static void ensure_default_theme_exists()
 {
     // Without a user tint2rc file, copy the default
-    gchar *path_home = g_build_filename((fetch_user_config_dir(), user_config_dir), "tint2", "tint2rc", NULL);
+    gchar *path_home = strdup_printf( NULL, "%s/tint2/tint2rc", (fetch_user_config_dir(), user_config_dir));
     if (!g_file_test(path_home, G_FILE_TEST_EXISTS)) {
         const gchar *const *system_dirs = g_get_system_config_dirs();
-        for (int i = 0; system_dirs[i]; i++) {
-            gchar *path = g_build_filename(system_dirs[i], "tint2", "tint2rc", NULL);
+        for (int i = 0; system_dirs[i]; i++)
+        {
+            gchar *path = strdup_printf( NULL, "%s/tint2/tint2rc", system_dirs[i]);
             if (g_file_test(path, G_FILE_TEST_EXISTS)) {
                 copy_file(path, path_home);
                 break;
             }
-            g_free(path);
+            free( path);
         }
     }
-    g_free(path_home);
+    free( path_home);
 }
 
 static int theme_file_valid (const char *file_name)
@@ -797,10 +797,10 @@ static int theme_file_valid (const char *file_name)
 static gboolean load_user_themes()
 {
     // Load configs from home directory
-    gchar *tint2_config_dir = g_build_filename((fetch_user_config_dir(), user_config_dir), "tint2", NULL);
+    gchar *tint2_config_dir = strdup_printf( NULL, "%s/tint2", (fetch_user_config_dir(), user_config_dir));
     GDir *dir = g_dir_open(tint2_config_dir, 0, NULL);
     if (dir == NULL) {
-        g_free(tint2_config_dir);
+        free( tint2_config_dir);
         return FALSE;
     }
     gboolean found_theme = FALSE;
@@ -810,13 +810,13 @@ static gboolean load_user_themes()
         if (theme_file_valid (file_name))
         {
             found_theme = TRUE;
-            gchar *path = g_build_filename(tint2_config_dir, file_name, NULL);
+            gchar *path = strdup_printf( NULL, "%s/%s", tint2_config_dir, file_name);
             theme_list_append(path);
-            g_free(path);
+            free( path);
         }
     }
     g_dir_close(dir);
-    g_free(tint2_config_dir);
+    free( tint2_config_dir);
 
     return found_theme;
 }
@@ -825,7 +825,7 @@ static gboolean load_themes_from_dirs(const gchar *const *dirs)
 {
     gboolean found_theme = FALSE;
     for (int i = 0; dirs[i]; i++) {
-        gchar *path_tint2 = g_build_filename(dirs[i], "tint2", NULL);
+        gchar *path_tint2 = strdup_printf( NULL, "%s/tint2", dirs[i]);
         GDir *dir = g_dir_open(path_tint2, 0, NULL);
         if (dir) {
             const gchar *file_name;
@@ -833,14 +833,14 @@ static gboolean load_themes_from_dirs(const gchar *const *dirs)
                 if (theme_file_valid (file_name))
                 {
                     found_theme = TRUE;
-                    gchar *path = g_build_filename(path_tint2, file_name, NULL);
+                    gchar *path = strdup_printf( NULL, "%s/%s", path_tint2, file_name);
                     theme_list_append(path);
-                    g_free(path);
+                    free( path);
                 }
             }
             g_dir_close(dir);
         }
-        g_free(path_tint2);
+        free( path_tint2);
     }
     return found_theme;
 }
@@ -858,11 +858,10 @@ static gboolean load_system_themes()
 static gchar *find_theme_in_dirs(const gchar *const *dirs, const gchar *given_name)
 {
     for (int i = 0; dirs[i]; i++) {
-        gchar *filepath = g_build_filename(dirs[i], "tint2", given_name, NULL);
-        if (g_file_test(filepath, G_FILE_TEST_EXISTS)) {
+        gchar *filepath = strdup_printf( NULL, "%s/tint2/%s", dirs[i], given_name);
+        if (g_file_test(filepath, G_FILE_TEST_EXISTS))
             return filepath;
-        }
-        g_free(filepath);
+        free( filepath);
     }
     return NULL;
 }

commit 2b1a10321dc978c4ab5cc4aeec72264e3ab192b1

typo - regression in 836e9e3: tray disappears when icon disappears

src/systray/systraybar.c
@@ -911,7 +911,7 @@ void remove_icon(TrayWindow *traywin)
     free( traywin);
 
     // check empty systray
-    if (systray.list_icons)
+    if (!systray.list_icons)
         hide(&systray.area);
 
     // Resize and redraw the systray